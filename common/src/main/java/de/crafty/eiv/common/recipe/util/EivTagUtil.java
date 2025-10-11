a56281623b883cd61f9c0bc165e48c78ace3e25b
Crafty-Dev <hohmann.lius@gmail.com>
2025-10-11 23:06:24 +0200

    [9] - updated to version 3.0.2 => fixed NPE

--- common/src/main/java/de/crafty/eiv/common/recipe/util/EivTagUtil.java
+++ common/src/main/java/de/crafty/eiv/common/recipe/util/EivTagUtil.java
@@ -62,6 +62,9 @@ public class EivTagUtil {
     }
 
     public static CompoundTag writeIngredient(Ingredient ingredient) {
+        if(ingredient == null)
+            return new CompoundTag();
+
         Either<TagKey<Item>, List<Holder<Item>>> ingredientContent = ((IngredientAccessor) (Object) ingredient).getValues().unwrap();
 
         CompoundTag tag = new CompoundTag();
@@ -76,6 +79,9 @@ public class EivTagUtil {
     }
 
     public static Ingredient readIngredient(CompoundTag tag) {
+        if(tag.isEmpty())
+            return null;
+
         if (tag.contains("tag")) {
             TagKey<Item> tagKey = TagKey.create(Registries.ITEM, ResourceLocation.parse(tag.getString("tag")));
             return Ingredient.of(Objects.requireNonNull(BuiltInRegistries.ITEM.get(tagKey).orElse(null)));
