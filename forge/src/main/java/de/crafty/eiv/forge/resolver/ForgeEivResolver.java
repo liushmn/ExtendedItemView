0ba17a97f752827e6ff5b4f640a6f05510fd5dee
Crafty-Dev <hohmann.lius@gmail.com>
2025-10-16 23:06:10 +0200

    [1.21.10-66] - fixed fluid rendering

--- forge/src/main/java/de/crafty/eiv/forge/resolver/ForgeEivResolver.java
+++ forge/src/main/java/de/crafty/eiv/forge/resolver/ForgeEivResolver.java
@@ -15,7 +15,7 @@ public class ForgeEivResolver implements IEivClientResolver {
 
     @Override
     public UVInfo getUVInfo(TextureAtlasSprite sprite) {
-        return new UVInfo(0, 1, 0, 1);
+        return new UVInfo(sprite.getU0(), sprite.getU1(), sprite.getV0(), sprite.getV1());
     }
 
 }
