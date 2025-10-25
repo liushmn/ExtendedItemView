0d074e620e89247dc6660e1cf43fe193eb99f482
Crafty-Dev <hohmann.lius@gmail.com>
2025-10-25 14:44:45 +0200

    [1.21.10-19] - polished integration discovery, finished version 1.0.0 of servercompat

--- servercompat/src/main/java/de/crafty/eiv/servercompat/EivPlugin.java
+++ servercompat/src/main/java/de/crafty/eiv/servercompat/EivPlugin.java
@@ -1,12 +1,20 @@
 package de.crafty.eiv.servercompat;
 
+import de.crafty.eiv.servercompat.api.IEivCompatIntegration;
 import de.crafty.eiv.servercompat.builtin.BuiltinEivCompatIntegration;
 import de.crafty.eiv.servercompat.event.PlayerJoinListener;
-import de.crafty.eiv.servercompat.event.ServerReloadListener;
 import de.crafty.eiv.servercompat.log.Data;
 import de.crafty.eiv.servercompat.recipe.CompatRecipeManager;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class EivPlugin extends JavaPlugin {
 
@@ -25,8 +33,12 @@ public class EivPlugin extends JavaPlugin {
 
         CompatRecipeManager.INSTANCE.registerIntegration(this, new BuiltinEivCompatIntegration());
 
+
+        //Discovering Integrations
+        this.discoverIntegrations();
+        CompatRecipeManager.INSTANCE.loadIntegrations();
+
         Bukkit.getPluginManager().registerEvents(new PlayerJoinListener(), this);
-        Bukkit.getPluginManager().registerEvents(new ServerReloadListener(), this);
 
         Data.log("Extended ItemView has been enabled.");
     }
@@ -44,4 +56,39 @@ public class EivPlugin extends JavaPlugin {
         return instance;
     }
 
+
+    private void discoverIntegrations() {
+        Data.log("Integration discovery started...");
+        HashMap<Plugin, Class<? extends IEivCompatIntegration>> integrationClasses = new HashMap<>();
+
+        for (Plugin plugin : Bukkit.getPluginManager().getPlugins()) {
+            if (!plugin.isEnabled()) continue;
+
+            if (plugin.getResource("plugin.yml") != null) {
+                YamlConfiguration config = YamlConfiguration.loadConfiguration(new InputStreamReader(plugin.getResource("plugin.yml")));
+
+                if (config.contains("eiv-compat")) {
+                    try {
+                        integrationClasses.put(plugin, (Class<? extends IEivCompatIntegration>) Class.forName(config.getString("eiv-compat")));
+
+                    } catch (ClassNotFoundException | ClassCastException e) {
+                        Data.error("Invalid eiv-compat class in plugin.yml, skipping plugin...");
+                    }
+                }
+
+            }
+        }
+        integrationClasses.forEach((plugin, integrationClass) -> {
+
+            try {
+                CompatRecipeManager.INSTANCE.registerIntegration(plugin, integrationClass.newInstance());
+            } catch (InstantiationException | IllegalAccessException e) {
+                Data.error("Invalid eiv-compat class in plugin.yml, skipping plugin...");
+            }
+
+        });
+
+        Data.log(String.format("Found %s EIV-Compat integration(s).", CompatRecipeManager.INSTANCE.getIntegrationCount()));
+
+    }
 }
